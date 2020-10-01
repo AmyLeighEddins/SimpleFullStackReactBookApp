c748beefdcac165f23021329d20edd987aa5f913
Amy Eddins <amyleigheddins@gmail.com>
2020-10-01 15:13:29 -0500

    added option to add a new book to the list

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -2,6 +2,27 @@ import React from "react";
 import "./App.css";
 function App() {
   const [books, setBooks] = React.useState([]);
+  const [value, setValue] = React.useState("");
+  const handleSubmit = event => {
+    event.preventDefault();
+  if (value && value !== "") {
+      const data = {
+        title: value
+      };
+      fetch("http://localhost:3001/books", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(result => {
+          setBooks(books.concat([result]));
+          setValue("");
+        });
+    }
+  };
 React.useEffect(() => {
     fetch("http://localhost:3001/books")
       .then(res => res.json())
@@ -18,6 +39,17 @@ return (
           <li key={book.id}>{book.title}</li>
         ))}
       </ul>
+      <form onSubmit={handleSubmit} className="new-form">
+      <input
+        className="new-input"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        placeholder="eg. The Great Gatsby"
+      />
+      <button className="button primary-button" type="submit">
+        Submit
+      </button>
+</form>
     </div>
   );
 }
